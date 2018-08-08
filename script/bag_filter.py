ce3a73d86399f2710fc9abbc944150e7422c4ecc
Bob Wang <wangziling1000@gmail.com>
2018-08-08 15:36:22 +0200

    add bag_filter

---
+++ script/bag_filter.py
@@ -0,0 +1,106 @@
+import re
+import os
+import logging
+
+
+def bag_filter(dir, names=[], logger=None):
+    # init 
+    exc_content = {}
+    is_part = False
+    b_get_bag = False
+
+    for name in names:
+        if dir[-1] != '/':
+            dir = dir + '/'
+
+        fn = dir+name
+        with open(fn, 'r') as f:
+            for line in f:
+                # init
+                b_get_bag = False
+
+                if line.startswith('tag'):
+                    p_day = '(^tag )(\d+)'
+                    result = re.match(p_day, line)
+                    day = result.groups()[1]
+                    week = None
+                    uid = None
+                elif line.startswith('woche'):
+                    p_week = '(^woche)(\d+)'
+                    result = re.match(p_week, line)
+                    week = result.groups()[1]
+
+                elif line.startswith('HEiKA'):
+                    p_uid = '(^HEiKA)(\d*)'
+                    result = re.match(p_uid, line)
+                    uid = result.groups()[1]
+
+                elif line.startswith('start_time'):
+                    p_start_time = '(^start_time: )(.*)'
+                    result = re.match(p_start_time, line)
+
+                elif line.startswith('e'):
+                    p_exc = '(^e)(\d)(.)(.*)'
+                    result = re.match(p_exc, line)
+                    char = result.groups()[2]
+                    num = result.groups()[1]
+                    if char == ':':
+                        content = result.groups()[3][1:]
+                        exc_content[num] = content
+
+                    elif char == ' ':
+                        b_get_bag = True
+                        exc_num = num
+                        infos = result.groups()[3].split(' ')
+
+                        if infos[1].startswith('part'):
+                            is_part = True
+                            exc_times = infos[0][1:]
+                            part_num = infos[1][-2]
+
+                        else:
+                            is_part = False
+
+                    if b_get_bag:
+                        week_txt = week+'te_Woche/'
+                        day_txt_dir = day+'_tag/'
+                        user_txt = 'heika'+uid+'_'
+                        day_txt = 'd'+day+'_'
+                        exc_txt = 'e'+exc_num+'_'+exc_times
+                        if is_part:
+                            exc_txt += '_p'+part_num
+
+                        bag_fn = dir+week_txt+day_txt_dir+user_txt+day_txt+exc_txt+'.bag'
+                        if os.path.isfile(bag_fn):
+                            logger.info(bag_fn)
+
+                        else:
+                            msg = "the file dosen't exit:"
+                            msg += msg+bag_fn
+                            logger.info(msg)
+
+
+def main(dir, conf_files=None):
+    logger_name = 'bag_filter'
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.INFO)
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    dir_path = os.path.join(dir_path, '..')
+    dir_path = os.path.join(dir_path, 'data/log')
+    logger_path = os.path.join(dir_path, logger_name+'.log')
+    fh = logging.FileHandler(logger_path)
+    fh.setLevel(logging.INFO)
+    fmt = fmt = "%(asctime)-15s %(levelname)-6s %(filename)-10s %(lineno)d \
+                            %(process)d %(message)-5s"
+    datafmt = "%d %b %Y %H:%M:%S"
+    formatter = logging.Formatter(fmt, datafmt)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+    print(logger_path)
+    if conf_files is None:
+        conf_files = ['tag 1']
+    return bag_filter(dir, conf_files, logger)
+
+
+if __name__ == '__main__':
+    main()
